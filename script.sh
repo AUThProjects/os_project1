53f09426d04a10a38856fb009288f2ddc4d53186
Antonis Anagnostou <anagnoad@csd.auth.gr>
2014-11-02 21:42:53 +0200

    Almost finished

--- script.sh
+++ script.sh
@@ -1,21 +1,26 @@
-#!/bin/bash -x
+#!/bin/bash
 
 # Initializing part
-ps xao pid,ppid,pgid,sid,comm | grep chromium | awk '{print $1}' > initialPids;
+ps xao pid,ppid,pgid,sid,comm | grep chromium | awk '{print $1}' > CTP_initialPids;
 while read id; do
 	kill -s SIGKILL $id
-done < initialPids;
+done < CTP_initialPids;
 
 # Opening tabs
 while read page; do
 	chromium-browser $page &
 	sleep 5s
-done < urls2 #url.in
-sleep 2s # 30s;
+done < url.in
+sleep 30s
 
-ps kstart_time -ef | grep [c]hromium | awk '{print $2}' | tac > sortedPids
+# Closing tabs in reverse start time
+ps kstart_time -ef | grep [c]hromium | awk '{print $2}' | tac > CTP_sortedPids
+cp CTP_sortedPids CTP_sortedPids2
 while read pid; do
+	echo $pid
 	kill -s SIGKILL $pid
 	sleep 5s
-	ps kstart_time -ef | grep [c]hromium | awk '{print $2}' | tac > sortedPids
-done < "`tail -n 1 sortedPids`"
+	#ps kstart_time -ef | grep [c]hromium | awk '{print $2}' | tac > CTP_sortedPids # to be asked
+done < CTP_sortedPids
+
+return $?
